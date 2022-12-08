57d537b3be5678df70c6bb3eae370906b5821f95
iktueren <iktuerensemih@gmail.com>
2022-12-08 22:32:08 +0300

    3rd Form added listing to a listbox via a smiple method

---
+++ OOP Intro/Form3.cs
@@ -0,0 +1,39 @@
+﻿using OOP_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Intro
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Educator educator = new Educator();
+            educator.Name = textBox1.Text;
+            educator.LastName = textBox2.Text;
+            educator.Age = textBox3.Text;
+
+            listBox1.Items.Add(educator.ReflectInfo());
+
+
+        }
+    }
+}
---
+++ OOP Intro/Models/Educator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Intro.Models
+{
+    internal class Educator
+
+    {
+
+        public string Name { get; set; }
+        public string LastName { set; get; }
+        public string Age { get; set; }
+        
+        public string ReflectInfo()
+        {
+            return $"{Name}{LastName}{Age}";
+        }
+
+    }
+}
