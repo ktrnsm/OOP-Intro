69c322ce5cb3cac6b87bebe53c5d1eb977486167
iktueren <iktuerensemih@gmail.com>
2022-12-08 21:47:32 +0300

    Add project files.

---
+++ OOP Intro/Form1.cs
@@ -0,0 +1,35 @@
+﻿using OOP_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Intro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Auto a = new Auto();
+            a.Name = "XYZ";
+            a.Type = "ABC";
+            a.Model = "CXZ";
+            a.Color = "blue";
+
+            Auto b = new Auto();
+            b.Name = "QWE";
+
+        }
+    }
+}
---
+++ OOP Intro/Models/Auto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Intro.Models
+{
+    internal class Auto
+    {
+
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+
+        public void Drive()
+        {
+            MessageBox.Show("Let's Goo!!");
+
+        }
+
+        public void Break()
+        {
+            MessageBox.Show("Please Stop");
+        }
+    }
+
+    
+}
